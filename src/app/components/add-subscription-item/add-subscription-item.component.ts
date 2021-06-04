d5db995d990f80b350ff77064f0c66a930e4f485
My Tran <mytran1109@gmail.com>
2021-06-03 20:53:45 -0700

    Add Create,Cancel button on fill form create item

--- src/app/components/add-subscription-item/add-subscription-item.component.ts
+++ src/app/components/add-subscription-item/add-subscription-item.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SubscriptionService } from '../../services/subscription.service';
+import { SubscriptionItem } from '../../models/ISubscriptionItemDetail';
+
 
 @Component({
   selector: 'app-add-subscription-item',
@@ -10,11 +15,78 @@ export class AddSubscriptionItemComponent implements OnInit {
   optionsRecur = ['Annually', 'Monthly', 'Daily'];
   optionsReminder = ['Email', 'Phone#'];
 
+  selectedSubType: string | undefined;
+  selectedRecur: string | undefined;
+  selectedReminder: string | undefined;
+
+  itemIdFromRoute: string | null | undefined;
+  itemDetail!: SubscriptionItem;
 
 
-  constructor() { }
+  constructor(
+    private subscriptionService: SubscriptionService,
+    private location: Location,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    this.itemIdFromRoute = routeParams.get('itemId');
+
+    // Call service for item detail
+    this.subscriptionService.getSubscriptionDetail(this.itemIdFromRoute).subscribe((subItem) => {
+      this.itemDetail = subItem;
+      switch (subItem.subscriptionType){
+        case 'Trial':
+          this.selectedSubType = this.optionsSubType[0];
+          break;
+        case 'Standard':
+          this.selectedSubType = this.optionsSubType[1];
+          break;
+        case 'Premium':
+          this.selectedSubType = this.optionsSubType[2];
+          break;
+        default:
+          this.selectedSubType = this.optionsSubType[3];
+      }
+
+      switch (subItem.recurringOption){
+        case 'Annually':
+          this.selectedRecur = this.optionsRecur[0];
+          break;
+        case 'Premium':
+          this.selectedRecur = this.optionsRecur[2];
+          break;
+        default:
+          this.selectedRecur = this.optionsRecur[1];
+          break;
+      }
+
+      switch (subItem.reminderMethod){
+        case 'Phone#':
+          this.selectedReminder = this.optionsReminder[1];
+          break;
+        default:
+          this.selectedReminder = this.optionsReminder[0];
+          break;
+      }
+    });
   }
 
+
+  onClickBack(): void {
+    this.location.back();
+  }
+
+  onClickSave(): void {
+    const itemId = String(this.itemIdFromRoute);
+    this.subscriptionService.updateSubscriptionDetail(itemId, this.itemDetail).subscribe((newItem) => {
+      console.log(newItem)
+    });
+    this.onClickBack();
+  }
 }
+
+
